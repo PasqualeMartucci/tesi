9ef6aa8226a7224033ab7a0859feb763e37afa51
Lebesgue97 <pasquale.martucci2@studenti.unicampania.it>
2020-04-21 15:58:17 +0200

    2.0

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int main(){
 int n;    
-
+int temp;
+int j;
 do{
 printf("Inserire la dimensione del vettore: ");    
 scanf("%d",&n);    
@@ -21,6 +22,28 @@ printf("%d ",V[i]);
 }    
     
     
+
+printf("\nadesso ordiniamo con insertion sort\n");
+		
+	for(int i=1;i<n;i++){ 
+            temp=V[i]; 
+            j=i-1; 
+            while(j>=0 && V[j]>temp){
+		V[j+1]=V[j];		
+		j--;		
+	}
+	V[j+1]=temp;
+	}
+
+
+
+
+
+
+
+
+
+
 }
     
     
